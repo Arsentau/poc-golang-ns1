dd363bfd8302fb9e0f2c21c6a503c2dedcd74e8f
alanmvarela <alan.martin.varela@gmail.com>
2023-01-09 14:44:47 -0300

    Fix linter error

--- internal/pkg/custom_api/controllers/networks.go
+++ internal/pkg/custom_api/controllers/networks.go
@@ -2,12 +2,18 @@ package controllers
 
 import (
 	"encoding/json"
+	"log"
 	"net/http"
 )
 
 func GetAllNetworks(w http.ResponseWriter, r *http.Request) {
 	// TODO: Implement Client
+	ns1_networks := []string{"network1", "network2"}
+
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	json.NewEncoder(w).Encode([]string{"network1", "network2"})
+
+	if err := json.NewEncoder(w).Encode(ns1_networks); err != nil {
+		log.Println("Failed to get Networks - ", err)
+	}
 }
