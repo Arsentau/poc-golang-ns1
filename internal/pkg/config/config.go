0fe8bf9aceadc312b0bc4f1fd371d258684443bc
Nabor Torres <arsentau@gmail.com>
2023-01-10 16:09:48 -0300

    Add error handling in viper.ReadInConfig()

--- internal/pkg/config/config.go
+++ internal/pkg/config/config.go
@@ -21,7 +21,10 @@ func GetConfig() *Config {
 	}
 
 	viper.SetConfigFile(".env")
-	viper.ReadInConfig()
+	e := viper.ReadInConfig()
+	if e != nil {
+		log.Panic("Viper: Error while reading config")
+	}
 	err := viper.Unmarshal(&config)
 	if err != nil {
 		log.Fatalf("Unable to decode into struct, %v", err)
