62eab0793599fc5f79c889fa8a801dcdb6f2cf5c
Arsentau <76915385+Arsentau@users.noreply.github.com>
2023-01-12 13:10:55 -0300

    Nabor/fix implementation details and document api (#6)
    * Improve error handling
    
    * Improve error handling

--- internal/pkg/official_api/controllers/zones.go
+++ internal/pkg/official_api/controllers/zones.go
@@ -3,10 +3,7 @@ package controllers
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
-	"strconv"
-	"strings"
 
 	sdk "github.com/poc-golang-ns1/internal/pkg/ns1-sdk"
 	e "github.com/poc-golang-ns1/pkg/errors"
@@ -14,20 +11,18 @@ import (
 
 // GetAllZonesHandler gets the zones using SDK implementation, if fails sends a custom HTTP error
 func GetAllZonesHandler(w http.ResponseWriter, r *http.Request) {
-	zones, err := sdk.GetZones()
+	w.Header().Set("Content-type", "application/json")
+	zones, code, err := sdk.GetZones()
 
 	if err != nil {
-		message := strings.Fields(err.Error())
-		code, err := strconv.Atoi(message[2])
-		if err != nil {
-			code = 500
-		}
-		e.ErrorHTTPResponse(w, message[1], code)
+		message := err.Error()
+		e.ErrorHTTPResponse(w, message, code)
 		return
 	}
-	w.Header().Set("Content-type", "application/json")
-	e := json.NewEncoder(w).Encode(&zones)
-	if e != nil {
-		log.Panic("Error while encoding response")
+
+	if err = json.NewEncoder(w).Encode(zones); err != nil {
+		e.ErrorHTTPResponse(w, err.Error(), 500)
+		return
 	}
+	w.WriteHeader(code)
 }
